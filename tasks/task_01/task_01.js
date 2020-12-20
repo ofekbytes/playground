d950862bf4674dbe680a9306f05f46ba197ceed3
ofekbytes <ofekbytes@gmail.com>
2020-12-20 18:15:32 +0200

    example

--- tasks/task_01/task_01.js
+++ tasks/task_01/task_01.js
@@ -18,6 +18,7 @@ function taskLinkedList() {
      **/
     var length = 0;
     var head = null;
+    var count = 0;
 
 
     /** 
@@ -79,16 +80,143 @@ function taskLinkedList() {
     /**
      * Node - element At
      **/
-    this.elementAt = function(index) {
+    this.elementAt = function (index) {
         var currentNode = head;
         var count = 0;
-        while (count < index){
-            count ++;
+        while (count < index) {
+            count++;
             currentNode = currentNode.next
         }
         return currentNode.element;
-      }; 
+    };
 
+
+    /**
+     * count all element in LinkedList (taskLinkedList)
+     **/
+    this.countLinks = function () {
+
+        var currentNode = head;
+        var tc = 1;
+
+        if (head === null) {
+            tc = (-1);
+        }
+        else {
+            while (currentNode.next) {
+                currentNode = currentNode.next;
+                tc++;
+            }
+        }
+        count = tc;
+
+        return tc;
+    }
+
+
+     /**
+     * printLinkedList - print all element in LinkedList (taskLinkedList)
+     **/
+    this.printLinkedList = function () {
+
+        var currentNode = head;
+        var tc = 1;
+        var index = 0;
+
+        if (head === null) {
+            tc = (-1);
+        }
+        else {
+
+            console.log(`print all linked list element `);
+            while (currentNode.next !== null) {
+                console.log(`_[ ${index} ]_ ===> ${currentNode.element}`);
+                currentNode = currentNode.next;
+            }    
+            console.log(`_[ ${index} ]_ ===> ${currentNode.element}`); //print the last element.
+
+        }
+        return tc;
+    }
+
+
+     /**
+     * printLinkedListOBO - print all element in LinkedList One By One (taskLinkedList)
+     **/
+    this.printLinkedListOBO = function () {
+
+            console.log(`print all linked list element One By One `);
+            var index = 0;
+
+            for (index = 0; index < count; index++) {
+                console.log(`[ ${index} ] ===> ${this.elementAt(index)}`)
+            }
+
+        return index;
+    }
+
+
+    /**
+     * printResult
+     * @param {*} tc  --> output variable.
+     **/
+    this.printResult = function (tc) {
+        console.log(`total nodes in linkedList:: <[${tc}]>`);
+
+    }
+
+
+    /**
+     * average - sum / count = average
+     **/
+    this.average = function() {
+
+        var averageValue = 0;
+        let sum = 0;
+
+        currentNode = head;
+
+        while (currentNode.next !== null) {
+            console.log(sum);
+            sum += currentNode.element ;
+            currentNode = currentNode.next;
+        }
+        sum += currentNode.element ;
+
+        averageValue = sum / count;
+        console.log(`${sum} / ${count} ===> ${averageValue}`);
+
+        return averageValue;
+    }
+
+
+    /**
+     * 
+     **/
+    this.insertValueIntoLinkedList = function(number) {
+        
+        var currentNode = head;
+        var sum = 0;
+        var numberOne = 0;
+
+        if (currentNode === null) {
+            return -1;
+        }
+        else {
+            while (currentNode !== null) {
+                numberOne = currentNode.element;        
+                sum = sum + numberOne;
+                // sum = sum + (currentNode.element + currentNode.next.element);
+                // console.log(" *** " + currentNode.element + " **** " + (numberOne + numberOne) );
+                currentNode = currentNode.next;
+            }
+            
+            console.log(`sum:: ${sum}`);
+        }
+    }
+    
+
+//// taskLinkedList ////
 }
 
 
@@ -96,20 +224,55 @@ function taskLinkedList() {
 var task = new taskLinkedList();
 
 
+task.printResult(`${task.countLinks()}`);
+
 task.add(10);
 task.add(20);
 task.add(30);
 
+task.printResult(`${task.countLinks()}`);
 
-console.log(`print all linked list element `);
-let sum = 0;
-for(let a = 0; a < task.size(); a++)
-{
-    console.log(`node [${a}] ==> [ ${task.elementAt(a)} ]`);
-    sum += task.elementAt(a);
-}
+///\n
+console.log();
 
-console.log(`nodes average value : [ ${sum / task.size()} ] `);
+// print Linked List
+task.printLinkedList();
+
+///\n
+console.log();
+
+// print Linked List - with index One By One
+task.printLinkedListOBO();
+
+// print total nodes, counting one by one
+console.log(`\n Total:: ${task.countLinks()}`);
+
+// print average
+console.log(`\n average:: ${task.average()}`);
+
+
+// TODO
+//
+// task.sumLinkedList();
+var number = 10;
+console.log('');
+console.log('check if two number have an average of number');
+task.insertValueIntoLinkedList(number);
+
+////----[*****]---------------------------------------------------------------////
+
+
+// DELETE  - after TODO task complite
+//
+// console.log(`print all linked list element `);
+// let sum = 0;
+// for (let a = 0; a < task.size(); a++) {
+//     console.log(`node [${a}] ==> [ ${task.elementAt(a)} ]`);
+//     sum += task.elementAt(a);
+// }
+
+// console.log(`nodes average value : [ ${sum / task.size()} ] `);
+
 
 
 //
